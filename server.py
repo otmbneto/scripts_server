a34f4fc7a476eb2f9ed5652564c9ebf1d5157500
otmbneto <otmbneto@protonmail.com>
2022-05-17 16:28:25 -0300

    Fixed global var reference

--- server.py
+++ server.py
@@ -1,23 +1,28 @@
 import os
 from oscpy.server import OSCThreadServer
 import scripts.renderfarm_services as srs
+import time
 
 rf = None
-
 def getRenderStatus(*args):
 
-	print(values)
+	global rf
+	print("Getting render status")
 	if rf is None:
 		queues = ["/Storage/MNM_OMeninoMaluquinho/MNM_TBLIB/__TEMP_sandbox/render_farm/folder_001","/Storage/MNM_OMeninoMaluquinho/MNM_TBLIB/__TEMP_sandbox/render_farm/folder_002"]
 		rf = srs.Renderfarm(os.getenv("OC_CLIENT"),os.getenv("OC_LOGIN"),os.getenv("OC_PASSWD"),queues)
 
 	output = rf.checkRenderStatus(args[0] if len(args) > 0 else "")
-	osc.answer(str.encode(output))
 
 
 osc = OSCThreadServer()  # See sources for all the arguments
 
 # You can also use an \*nix socket path here
-sock = osc.listen(address='0.0.0.0', port=8000, default=True)
-
+sock = osc.listen(address='127.0.0.1', port=8030, default=True)
 osc.bind(b'/check', getRenderStatus)
+
+while(True):
+
+	print("Listening...")
+	time.sleep(5)
+
